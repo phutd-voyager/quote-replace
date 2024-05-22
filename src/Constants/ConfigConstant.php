b6211d4f464c752c2c8cbadab3066cc4228a4710
Phu Tran Dinh <phutd@voyager-hcm.com>
2024-05-22 16:27:29 +0700

    Initial Code

---
+++ src/Constants/ConfigConstant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace VoyagerInc\QuoteReplace\Constants;
+
+class ConfigConstant
+{
+    const ENABLED_DEFAULT = true;
+
+    const FULL_WIDTH_QUOTES_DEFAULT = ['“', '”'];
+
+    const HALF_WIDTH_QUOTE_DEFAULT = '"';
+}
--- src/Services/ConfigDataService.php
+++ src/Services/ConfigDataService.php
@@ -2,22 +2,23 @@
 
 namespace VoyagerInc\QuoteReplace\Services;
 
+use VoyagerInc\QuoteReplace\Constants\ConfigConstant;
 use VoyagerInc\QuoteReplace\Contracts\ConfigDataServiceInterface;
 
 class ConfigDataService implements ConfigDataServiceInterface
 {
     public function getStatusPackage(): bool
     {
-        return config('quote_replace.enabled');
+        return config('quote_replace.enabled', ConfigConstant::ENABLED_DEFAULT);
     }
 
     public function getFullWidthQuotes(): array
     {
-        return config('quote_replace.full_width_quotes');
+        return config('quote_replace.full_width_quotes', ConfigConstant::FULL_WIDTH_QUOTES_DEFAULT);
     }
 
     public function getHalfWidthQuote(): string
     {
-        return config('quote_replace.half_width_quote');
+        return config('quote_replace.half_width_quote', ConfigConstant::HALF_WIDTH_QUOTE_DEFAULT);
     }
 }
